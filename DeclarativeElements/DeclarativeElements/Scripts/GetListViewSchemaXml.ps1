b36c42f849854e0254d7ee7d76c122aad1c66360
Adam Wójcik <adam.wojcik.it@gmail.com>
2019-12-29 23:47:52 +0100

    added more field types, added content types added list and libs defenitions added powershell helper to get schema

---
+++ DeclarativeElements/DeclarativeElements/Scripts/GetListViewSchemaXml.ps1
@@ -0,0 +1,21 @@
+﻿<#
+this script may be helpful to get schema xml of view that was created manually
+#>
+
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
+{
+    Add-PSSnapin "Microsoft.SharePoint.PowerShell"
+}
+
+$web = Get-SPWeb -site [URL]
+$list = $web.Lists.TryGetList("Element List 1")
+if ($list) 
+{
+    foreach ($view in $list.views) 
+    {
+        if ($view.Title -eq "CustomView1")
+        {
+            $view.HtmlSchemaXml
+        }
+    }
+}
