92172ae80e905261bfbef8adb23f0721e75b7b6e
Adam Wójcik <adam.wojcik.it@gmail.com>
2020-02-23 23:20:40 +0100

    added usercontrol, added webpart, added js file .. all used in the masterpage

---
+++ MasterPage/Components/ControlTemplates/Components/ExampleUserControl.ascx.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Web.UI;
+
+namespace Components.ControlTemplates.Components
+{
+    public partial class ExampleUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LbUserLoginName.Text = SPContext.Current.Web.CurrentUser.LoginName;
+        }
+    }
+}
---
+++ MasterPage/Components/Webpart/ExampleVisualWebPart/ExampleVisualWebPartUserControl.ascx.designer.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Components.Webpart.ExampleVisualWebPart {
+    
+    
+    public partial class ExampleVisualWebPartUserControl {
+        
+        /// <summary>
+        /// Kontrolka LbWebTitle.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LbWebTitle;
+        
+        /// <summary>
+        /// Kontrolka ExampleUserControl.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.UserControl ExampleUserControl;
+    }
+}
