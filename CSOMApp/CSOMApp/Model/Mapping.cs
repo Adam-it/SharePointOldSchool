b65a02540ddde4f9040a2f2abdd79f98c4eb497f
Adam Wójcik <adam.wojcik.it@gmail.com>
2020-03-07 00:06:19 +0100

    .

--- CSOMApp/CSOMApp/Model/Mapping.cs
+++ CSOMApp/CSOMApp/Model/Mapping.cs
@@ -2,7 +2,7 @@
 {
     public static class Mapping
     {
-        public const string URL = "https://testowy.local.umed.pl/sites/AdamTest";
+        public const string URL = "[URL]";
         public const string RELATIVE_URL = "/sites/AdamTest";
         public const string LIST_NAME = "TestListCSOM";
         public const string LIB_NAME = "TestLibCSOM";
